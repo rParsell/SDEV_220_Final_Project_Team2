52885dbd52a25f4949cae1e31041a6dfbd116b62
RJ Parsell <rparsell@ivytech.edu>
2025-04-17 20:24:21 -0400

    TestData Commit 5

--- testData.py
+++ testData.py
@@ -5,6 +5,9 @@ fileName = "employees.csv"
 fields = ['Employee ID','First Name','Last Name','Shift','Hours','Department','Sunday Available',
           'Monday Available','Tuesday Available','Wednesday Available', 'Thursday Available',
            'Friday Available', 'Saturday Available' ]
+
+
+
 rows = [  ['001','fName1','lName1','1', '40', 'Customer Service', 'True','False','True','True','True','False','True'],
           ['002','fName1','lName1','1', '40', 'Customer Service', 'True','True','True','True','False','True','True'],
           ['003','fName1','lName1','1', '40', 'Customer Service', 'True','True','False','True','True','True','True'],
@@ -15,17 +18,18 @@ rows = [  ['001','fName1','lName1','1', '40', 'Customer Service', 'True','False'
           ['008','fName1','lName1','2', '40', 'Customer Service', 'True','False','True','True','True','True','True'],
           ['009','fName1','lName1','2', '40', 'Customer Service', 'True','True','True','False','True','True','True'],
           ['010','fName1','lName1','2', '40', 'Customer Service', 'True','True','False','True','True','True','True'],
-          ['011','fName1','lName1','1', '40', 'Grocery', 'True','True','True','True','True','True','True'],
-          ['012','fName1','lName1','1', '40', 'Grocery', 'True','True','True','True','True','True','True'],
-          ['013','fName1','lName1','1', '40', 'Grocery', 'True','True','True','True','True','True','True'],
+          ['011','fName1','lName1','1', '40', 'Grocery', 'True','True','True','True','True','False','True'],
+          ['012','fName1','lName1','1', '40', 'Grocery', 'True','True','False','True','True','True','True'],
+          ['013','fName1','lName1','1', '40', 'Grocery', 'True','True','True','True','True','True','False'],
           ['014','fName1','lName1','1', '40', 'Grocery', 'False','True','True','True','True','True','True'],
           ['015','fName1','lName1','2', '40', 'Grocery', 'True','True','True','True','True','True','True'],
           ['016','fName1','lName1','2', '40', 'Grocery', 'True','True','True','False','True','True','True'],
-          ['017','fName1','lName1','2', '40', 'Grocery', 'True','True','True','True','True','True','True'],
-          ['018','fName1','lName1','3', '40', 'Grocery', 'True','True','True','True','True','True','True'],
-          ['019','fName1','lName1','3', '40', 'Grocery', 'True','True','True','True','True','True','True'],
+          ['017','fName1','lName1','2', '40', 'Grocery', 'True','False','True','True','True','True','True'],
+          ['018','fName1','lName1','3', '40', 'Grocery', 'True','False','True','True','True','True','True'],
+          ['019','fName1','lName1','3', '40', 'Grocery', 'True','True','True','True','False','True','True'],
           ['020','fName1','lName1','3', '40', 'Grocery', 'False','True','True','True','True','True','True'],
         ]
+
 with open(fileName, 'w') as csvfile:
     # creating a csv writer object
     csvwriter = csv.writer(csvfile)
@@ -43,8 +47,9 @@ with open(fileName, 'r') as csvfile:
     fields = next(csvreader)
 
     # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
+    # remove hashes below when you want to update data
+   # for row in csvreader:
+   #     rows.append(row)
 
     # get total number of rows
     print("Total no. of rows: %d" % (csvreader.line_num))
@@ -53,8 +58,8 @@ with open(fileName, 'r') as csvfile:
 print('Field names are:' + ', '.join(field for field in fields))
 
 # printing first 5 rows
-print('\nFirst 5 rows are:\n')
-for row in rows[:5]:
+print('\nPrinting Rows:\n')
+for row in rows:
     for col in row:
         print("%10s" % col, end=" "),
     print('\n')
